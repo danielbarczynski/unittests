b8c72f355f62ecd1a4229422e7000eb7d16f9b7c
Daniel Barczyński <daniel.barczynski@onet.pl>
2022-11-19 22:17:12 +0100

    update

--- BMI.cs
+++ BMI.cs
@@ -4,9 +4,24 @@ Console.WriteLine(result);
 
 public class BMI
 {
+    public enum BMIClassification 
+    {
+        Underweight,
+        Normal,
+        Overweight,
+        Obesity
+    }
     public double CalculateBMI(double height, double weight)
     {
         return Math.Round(weight / (height / 100 * height / 100), 2);
     }
+
+    public BMIClassification DetermineBMI(double bmi) => bmi switch
+    {
+        < 18.5 => BMIClassification.Underweight,
+        < 24.9 => BMIClassification.Normal,
+        < 29.9 => BMIClassification.Overweight,
+        _ => BMIClassification.Obesity
+    };
 }
 
--- Tests/BMITests.cs
+++ Tests/BMITests.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 public class BMITests
 {
     [Test]
-    public void Test() 
+    public void Assert_ForHeighAndWeght_Returns2137() 
     {
         // arrange
         var bmiCalculator = new BMI();
@@ -14,5 +14,15 @@ public class BMITests
 
         // assert
         Assert.AreEqual(result, 21.37);
+    }
+
+    [Test]
+    public void Determine_BMI()
+    {
+        double num = 21.37;
+        var determineBMI = new BMI();
+        var result = determineBMI.DetermineBMI(num);
+        Assert.AreEqual(result, BMI.BMIClassification.Normal); // if somebody change this classification to e.g. overweight, test will show an error
+
     }
 }
