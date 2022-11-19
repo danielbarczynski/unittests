8d65a8d2ea2dfddabab3a926c91605354ca5f529
Daniel Barczyński <daniel.barczynski@onet.pl>
2022-11-19 19:11:12 +0100

    update

--- Tests/BMITests.cs
+++ Tests/BMITests.cs
@@ -1,4 +1,18 @@
+using NUnit.Framework;
 public class BMITests
 {
-    
+    [Test]
+    public void Test() 
+    {
+        // arrange
+        var bmiCalculator = new BMI();
+        double height = 181.0;
+        double weight = 70.0;
+        
+        // act
+        var result = bmiCalculator.CalculateBMI(height, weight);
+
+        // assert
+        Assert.AreEqual(result, 21.37);
+    }
 }
