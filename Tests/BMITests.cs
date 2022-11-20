e01ca712fd392a40f56ac1c76b42c82a0795fb5e
Daniel Barczyński <daniel.barczynski@onet.pl>
2022-11-20 18:17:21 +0100

    update

--- Tests/BMITests.cs
+++ Tests/BMITests.cs
@@ -15,7 +15,7 @@ public class BMITests
         // assert
         Assert.AreEqual(result, 21.37);
     }
-
+    // one way (bad approach)
     [Test]
     public void Determine_BMI()
     {
@@ -23,6 +23,18 @@ public class BMITests
         var determineBMI = new BMI();
         var result = determineBMI.DetermineBMI(num);
         Assert.AreEqual(result, BMI.BMIClassification.Normal); // if somebody change this classification to e.g. overweight, test will show an error
+    }
 
+    // second way (good aproach)
+    [Test]
+    [TestCase(16.12, BMI.BMIClassification.Underweight)]
+    [TestCase(21.37, BMI.BMIClassification.Normal)]
+    [TestCase(25.99, BMI.BMIClassification.Overweight)]
+    [TestCase(36.66, BMI.BMIClassification.Obesity)]
+    public void Determine_BMI2(double num, BMI.BMIClassification bmiClassification)
+    {
+        var determineBMI = new BMI();
+        var result = determineBMI.DetermineBMI(num);
+        Assert.AreEqual(result, bmiClassification); 
     }
 }
